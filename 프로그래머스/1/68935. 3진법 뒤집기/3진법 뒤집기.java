46873be4ade1ffc1207be6026fe4c4c387fd0f29
MinJun Song <77713554+Luta13@users.noreply.github.com>
2024-07-12 14:49:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.11 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = null;
+        s = Integer.toString(n, 3);
+        StringBuffer sb = new StringBuffer(s);
+        String reverse = sb.reverse().toString();
+        answer = Integer.parseInt(reverse, 3);
+
+
+
+
+        return answer;
+    }
+}
